ebfbdb5356af5a4d7289e0c7a7075faa328d949b
Alex Jabbour <redpod22@gmail.com>
2019-10-21 23:12:57 -0700

    Add files via upload

---
+++ whatsapp/send_sms.py
@@ -0,0 +1,18 @@
+from twilio.rest import Client
+
+account_sid = 'AC3b3e28a38edee468497adc563689ca2b'
+auth_token = '269e439a8235f12e6767c109608aeaff'
+client = Client(account_sid, auth_token)
+
+# this is the Twilio sandbox testing number
+from_whatsapp_number='whatsapp:+14155238886'
+# replace this number with your own WhatsApp Messaging number
+to_whatsapp_number='whatsapp:+14164276256'
+
+message = 'Wagawn my g, its yaa boiiiii. I wrote this using a python script'
+
+client.messages.create(body=message,
+                       from_=from_whatsapp_number,
+                       to=to_whatsapp_number)
+
+print (message.sid)
